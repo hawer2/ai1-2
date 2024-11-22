3dd2ebd76ab5dd1c3ec42a24c9faa5b950b2ba29
hawer2 <20242501@sdhs.gwe.hs.kr>
2024-11-22 09:13:15 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,7 +47,7 @@ def display_right_content(prediction, data):
     # 2nd Row - YouTube Videos
     for i in range(3):
         with cols[i]:
-            st.video(data['https://www.youtube.com/watch?v=aVxP8_etVfg'][i])
+            st.video(data['https://youtu.be/aVxP8_etVfg?si=XPlfZwXvWlm2Guue'][i])
             st.caption(f"유튜브: {prediction}")
     # 3rd Row - Text
     for i in range(3):
